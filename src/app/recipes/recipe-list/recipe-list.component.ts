4fbbfb658461a348502f7364798ae684e926436c
Derek Moore <moore8577@gmail.com>
2019-08-01 21:57:47 -0700

    I understand all the Angulars

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "this is simply a test, maybe",
+      "https://easybaked.files.wordpress.com/2016/08/emoji-poo-cookies4.jpg"
+    ),
+    new Recipe(
+      "2nd Test Recipe",
+      "this is simply another test, maybe",
+      "https://www.tasteofhome.com/wp-content/uploads/2018/03/exps173967_HC163708B11_04_5b-696x696.jpg"
+    )
+  ];
 
   constructor() {}
 
